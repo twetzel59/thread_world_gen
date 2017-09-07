042ad2d05a603a23ad008439c4c25122cf55f1bf
twetzel59 <twetzel59@gmail.com>
2017-09-06 20:07:36 -0400

    Set up basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate sfml;
+
+use sfml::graphics::*;
+use sfml::window::{ContextSettings, Event, Key, VideoMode, style};
+
+const VIDEO_MODE: VideoMode = VideoMode { width: 800, height: 500, bits_per_pixel: 32 };
+
 fn main() {
-    println!("Hello, world!");
+    let mut win = create_window();
+
+    'outer: loop {
+        win.clear(&Color::rgb(85, 190, 255));
+        win.display();
+
+        while let Some(e) = win.poll_event() {
+            match e {
+                Event::KeyPressed { code: Key::Escape, .. } => break 'outer,
+                Event::Closed => break 'outer,
+                _ => {},
+            };
+        }
+    }
+}
+
+fn create_window() -> RenderWindow {
+    RenderWindow::new(VIDEO_MODE,
+                      "Multithreaded Worldgen",
+                      style::DEFAULT,
+                      &ContextSettings::default())
+                  .unwrap()
 }
